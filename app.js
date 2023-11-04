234109951e4e470fa2cc287f7f1e33b87f692bb1
cuba900 <armandobaro@gmail.com>
2023-11-04 15:36:18 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("O Andy fez esse commit sem voce ver na aula!")
+// Outro console log
+console.log("Aqui vai ser o que eu quiser, mas não vou mostrar pra vocês.")
